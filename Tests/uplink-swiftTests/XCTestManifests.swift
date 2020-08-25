7c8983c5fba2e774e78f7ddd0868be493b1b905f
Ayush Srivastava <55537146+ayush-srivastava-03@users.noreply.github.com>
2020-08-25 19:36:09 +0530

    Update XCTestManifests.swift

--- Tests/uplink-swiftTests/XCTestManifests.swift
+++ Tests/uplink-swiftTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(uplink_swiftTests.allTests)
+        testCase(UplinkswiftTests.allTests)
     ]
 }
 #endif
